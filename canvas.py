aad72a9f7223e1f04e94527a29d975683634da9b
Michael McThrow <mmcthrow@gmail.com>
2021-02-17 15:13:08 -0800

    Added code for diffuse reflection per Gambetta's Computer Graphics from Scratch text, but there's a problem with the output colors.

--- canvas.py
+++ canvas.py
@@ -8,7 +8,15 @@ class Color:
         self.blue = blue
 
     def __str__(self):
-        return "{:02x}{:02x}{:02x}".format(self.red, self.green, self.blue)
+        return "{:02x}{:02x}{:02x}".format(int(self.red), int(self.green), int(self.blue))
+
+    def vector(self):
+        return (self.red, self.green, self.blue)
+
+    def clamp(self):
+        self.red = min(255, max(0, self.red))
+        self.green = min(255, max(0, self.green))
+        self.blue = min(255, max(0, self.blue))
 
 class Pixel:
     def __init__(self, color):
@@ -37,41 +45,71 @@ class Canvas:
                 namesToColors[colorCount] = colorStr
                 colorCount += 1
 
+        print("Number of Unique Colors: {}".format(colorCount))
+        print("Colors: {}".format(list(map(lambda c: "#{}".format(c), colors.keys()))))
+
         return (colors, namesToColors)
 
     def putPixel(self, x, y, color):
-        print("x = {}, y = {}".format(x,y))
+        #print("x = {}, y = {}".format(x,y))
         index = self.toSy(y) * self.height + self.toSx(x)
-        print("index = {}".format(index))
+        #print("index = {}".format(index))
         self.pixels[index].color = color
 
-    def convertToXPM(self, filename):
-        formatHeader = "#define XFACE_format 1"
-        widthHeader = "#define XFACE_width {}".format(self.width)
-        heightHeader = "#define XFACE_height {}".format(self.height)
-        colorsToNames, namesToColors = self.colors()
-        numColorsHeader = "#define XFACE_ncolors {}".format(len(colorsToNames))
+    def computeCharsNeeded(self, colorsToNames):
         charsNeeded = 0
         currentLength = len(colorsToNames)
         while currentLength > 0:
             charsNeeded += 1
             currentLength = currentLength // 10
 
-        charsPerPixelHeader = "#define XFACE_chars_per_pixel {}".format(charsNeeded)
-        colorsEntry = "static char *XFACE_colors[] = {{\n{}\n}};".format(",\n".join(["\"{}\", \"#{}\"".format(str(name).zfill(charsNeeded), code) for name, code in namesToColors.items()]))
-        pixelsEntryFormat = "static char *XFACE_pixels[] = {{\n{}\n}};"
+        return charsNeeded
+
+    def makePixelsStr(self, charsNeeded, colorsToNames, xpmVersion):
         pixels = []
         for y in range(self.height):
             row = "";
             for x in range(self.width):
                 row += str(colorsToNames[str(self.pixels[y * self.height + x].color)]).zfill(charsNeeded)
-            pixels.append("\"{}\"".format(row))
-        pixelStr = ",\n".join(pixels)
-        pixelsEntry = pixelsEntryFormat.format(pixelStr)
+            if xpmVersion == 1:
+                pixels.append("\"{}\"".format(row))
+            else:
+                pixels.append("{}".format(row))
+        if xpmVersion == 1:
+            pixelsStr = ",\n".join(pixels)
+        else:
+            pixelsStr = "\n".join(pixels)
+        return pixelsStr
+
+    def convertToXPM1(self, filename):
+        formatHeader = "#define XFACE_format 1"
+        widthHeader = "#define XFACE_width {}".format(self.width)
+        heightHeader = "#define XFACE_height {}".format(self.height)
+        colorsToNames, namesToColors = self.colors()
+        numColorsHeader = "#define XFACE_ncolors {}".format(len(colorsToNames))
+        charsNeeded = self.computeCharsNeeded(colorsToNames)
+        charsPerPixelHeader = "#define XFACE_chars_per_pixel {}".format(charsNeeded)
+        colorsEntry = "static char *XFACE_colors[] = {{\n{}\n}};".format(",\n".join(["\"{}\", \"#{}\"".format(str(name).zfill(charsNeeded), code) for name, code in namesToColors.items()]))
+        pixelsEntryFormat = "static char *XFACE_pixels[] = {{\n{}\n}};"
+        pixelsStr = self.makePixelsStr(charsNeeded, colorsToNames, 1)
+        pixelsEntry = pixelsEntryFormat.format(pixelsStr)
 
         outputFile = open(filename, 'w')
         outputFile.write(formatHeader + '\n' + widthHeader + '\n' +
                          heightHeader + '\n' + numColorsHeader + '\n' +
                          charsPerPixelHeader + '\n' + colorsEntry + '\n' +
                          pixelsEntry)
+        outputFile.close()
+
+    def convertToXPM2(self, filename):
+        formatHeader = "! XPM2"
+        colorsToNames, namesToColors = self.colors()
+        charsNeeded = self.computeCharsNeeded(colorsToNames)
+        defHeader = "{} {} {} {}".format(self.width, self.height, len(colorsToNames), charsNeeded)
+        colorsEntry = "\n".join(["{} c #{}".format(str(name).zfill(charsNeeded), code) for name, code in namesToColors.items()])
+        pixelsStr = self.makePixelsStr(charsNeeded, colorsToNames, 2)
+        
+        outputFile = open(filename, 'w')
+        outputFile.write(formatHeader + '\n' + defHeader + '\n' +
+                         colorsEntry + '\n' + pixelsStr)
         outputFile.close()
--- raytrace.py
+++ raytrace.py
@@ -9,12 +9,41 @@ class Sphere:
         self.radius = radius
         self.color = color
 
+class Light:
+    def __init__(self, lightType, intensity, position=None, direction=None):
+        self.lightType = lightType
+        self.intensity = intensity
+        self.position = position
+        self.direction = direction
+
 def dot(u, v):
     total = 0
     for i in range(len(u)):
         total += u[i] * v[i]
     return total
 
+def scalarMultiply(k, v):
+    newV = [0] * len(v)
+
+    for i in range(len(v)):
+        newV[i] = v[i] * k
+
+    return tuple(newV)
+
+# Perform x - y on vectors
+def vectorSum(x, y, difference=False):
+    assert len(x) == len(y), "Vectors don't have the same length"
+
+    vSum = [0] * len(x)
+
+    for i in range(len(x)):
+        if difference:
+            vSum[i] = x[i] - y[i]
+        else:
+            vSum[i] = x[i] + y[i]
+
+    return tuple(vSum)
+    
 def main():
     width = 600
     height = 600
@@ -24,10 +53,16 @@ def main():
     spheres.append(Sphere((0, -1, 3), 1, canvas.Color(255, 0, 0)))
     spheres.append(Sphere((2, 0, 4), 1, canvas.Color(0, 0, 255)))
     spheres.append(Sphere((-2, 0, 4), 1, canvas.Color(0, 255, 0)))
+    spheres.append(Sphere((0, -5001, 0), 5000, canvas.Color(255, 255, 0)))
 
     viewportWidth = 1
     viewportHeight = 1
     viewportZ = 1
+
+    lights = []
+    lights.append(Light("ambient", 0.2))
+    lights.append(Light("point", 0.6, (2, 1, 0)))
+    lights.append(Light("directional", 0.2, None, (1, 4, 4)))
 
     def canvasToViewport(x, y):
         return (x * viewportWidth / width, y * viewportHeight / height,
@@ -35,9 +70,7 @@ def main():
 
     def intersectRaySphere(origin, direction, sphere):
         r = sphere.radius
-        circle_direction = (origin[0] - sphere.center[0],
-                            origin[1] - sphere.center[1],
-                            origin[2] - sphere.center[2])
+        circle_direction = vectorSum(origin, sphere.center, True)
 
         a = dot(direction, direction)
         b = 2 * dot(circle_direction, direction)
@@ -47,10 +80,28 @@ def main():
         if discriminant < 0:
             return (float('inf'), float('inf'))
 
-        t1 = (-b + math.sqrt(discriminant)) // (2 * a)
-        t2 = (-b - math.sqrt(discriminant)) // (2 * a)
+        t1 = (-b + math.sqrt(discriminant)) / (2 * a)
+        t2 = (-b - math.sqrt(discriminant)) / (2 * a)
 
         return (t1, t2)
+
+    def computeLighting(position, normal):
+        i = 0.0
+
+        for light in lights:
+            if light.lightType == "ambient":
+                i += light.intensity
+            else:
+                if light.lightType == "point":
+                    L = vectorSum(light.position, position, True)
+                else:
+                    L = light.direction
+ 
+                nDotL = dot(normal, L)
+                if nDotL > 0:
+                    i += light.intensity * nDotL / (len(normal) * len(L))
+
+        return i
 
     def traceRay(origin, direction, tMin, tMax):
         closestT = float('inf')
@@ -58,26 +109,34 @@ def main():
 
         for sphere in spheres:
             t1, t2 = intersectRaySphere(origin, direction, sphere)
-            if t1 >= tMin and t1 <= tMax and t1 < closestT:
+            if t1 > tMin and t1 < tMax and t1 < closestT:
                 closestT = t1
                 closestSphere = sphere
-            if t2 >= tMin and t2 <= tMax and t2 < closestT:
+            if t2 > tMin and t2 < tMax and t2 < closestT:
                 closestT = t2
                 closestSphere = sphere
 
         if not closestSphere:
             return canvas.Color(255, 255, 255)
 
-        return closestSphere.color
+        position = vectorSum(origin, scalarMultiply(closestT, direction))
+        normal = vectorSum(position, closestSphere.center, True)
+        normal = scalarMultiply(1.0 / len(normal), normal)
+
+        finalColor = scalarMultiply(computeLighting(position, normal),
+                                    closestSphere.color.vector())
+
+        return canvas.Color(finalColor[0], finalColor[1], finalColor[2])
 
     origin = (0, 0, 0)
     for x in range(-width // 2, width // 2):
         for y in range(-height // 2, height // 2):
             direction = canvasToViewport(x, y)
             color = traceRay(origin, direction, 1, float('inf'))
+            color.clamp()
             imageCanvas.putPixel(x, y, color)
 
-    imageCanvas.convertToXPM("raytracer.xpm")
+    imageCanvas.convertToXPM2("raytracer_v2.xpm")
 
 if __name__ == '__main__':
     main()
